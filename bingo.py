3f4c235e29805fb6d3d4ccee9ab65d99396cb7d9
Thiago Almeida <thiago9864@hotmail.com>
2022-12-18 02:06:26 -0300

    Python: Bingo versão 0.1

--- bingo.py
+++ bingo.py
@@ -7,6 +7,58 @@ import random
 
 # ----------------------- Definições ---------------------------
 
+def imprime_cartela_aux_item(lista_item, num_sorteado, n):
+    if(n == 0):
+        return []
+    head_item = lista_item[0]
+    tail_item = lista_item[1:]
+    if head_item['ja_saiu'] == True:
+        saiu = "X"
+    else:
+        saiu = " "
+    if head_item['numero'] < 10:
+        print(str(head_item['numero']) + "  ["+saiu+"]", end ="\t")
+    else:
+        print(str(head_item['numero']) + " ["+saiu+"]", end ="\t")
+
+    imprime_cartela_aux_item(tail_item, num_sorteado, n-1)
+
+
+def imprime_cartela_aux_linha(lista_linha, num_sorteado, n):
+    if(n == 0):
+        return
+    
+    head_linha = lista_linha[0]
+    tail_linha = lista_linha[1:]
+    imprime_cartela_aux_item(
+        head_linha, 
+        num_sorteado, 
+        len(head_linha)
+    )
+    print("")
+    imprime_cartela_aux_linha(tail_linha, num_sorteado, n-1)
+
+
+def imprime_cartela(lista_jogadores, num_sorteado, n):
+    if(n==0):
+        return
+    if(len(lista_jogadores)==n and num_sorteado > 0):
+        print("============================= Valor Sorteado: "+str(num_sorteado)+" ============================")
+    print("Cartelas:")
+    head_jogador = lista_jogadores[0]
+    tail_jogador = lista_jogadores[1:]
+
+    if head_jogador['tipo_cartela'] == 'l':
+        print ("============================= "+head_jogador['nome']+" : linha ===================================")
+    else:
+        print ("============================= "+head_jogador['nome']+" : coluna ===================================")
+    
+    imprime_cartela_aux_linha(
+        head_jogador['cartela'], 
+        num_sorteado, 
+        len(head_jogador['cartela'])
+    )
+    imprime_cartela(tail_jogador, num_sorteado, n-1)
 
 # ---------------------- Inicialização -------------------------
 
@@ -24,6 +76,10 @@ def inicia_jogadores_aux(num_jogadores, jogador):
     print("Digite o tipo da cartela de " + nome +
           " -- ( l ) para linha e ( c ) para coluna: ")
     tipo_cartela = input()
+
+    if tipo_cartela != 'l' and tipo_cartela != 'c':
+        print("Tipo de cartela inválido. Digite ( l ) para linha e ( c ) para coluna.")
+        inicia_jogadores_aux(num_jogadores, jogador)
 
     l1 = list(range(1, 16))
     l2 = list(range(16, 31))
@@ -58,6 +114,9 @@ def linha_cartela(n, lista):
 
 # ------------------------- Turnos -----------------------------
 
+##############################
+# Faz a atualização da cartela 
+##############################
 
 def preenche_cartela_aux_item(lista_item, num_sorteado, n):
     if(n == 0):
@@ -89,7 +148,44 @@ def preenche_cartela(lista_jogadores, num_sorteado, n):
     tail_jogador = lista_jogadores[1:]
     new_cartela = preenche_cartela_aux_linha(
         head_jogador['cartela'], num_sorteado, len(head_jogador['cartela']))
-    return [{'cartela': new_cartela }] + preenche_cartela(tail_jogador, num_sorteado, n-1)
+    return [{'nome':head_jogador['nome'], 'tipo_cartela': head_jogador['tipo_cartela'], 'cartela': new_cartela }] + preenche_cartela(tail_jogador, num_sorteado, n-1)
+
+##################################
+# Verifica se algum jogador ganhou 
+##################################
+
+def verifica_vitoria_aux_linha2(lista_item, n):
+    if(n == 0):
+        return True
+    head_item = lista_item[0]
+    tail_item = lista_item[1:]
+    return head_item['ja_saiu'] and verifica_vitoria_aux_linha2(tail_item, n-1)
+
+def verifica_vitoria_aux_linha(lista_linha, n):
+    if(n == 0):
+        return False
+    head_linha = lista_linha[0]
+    tail_linha = lista_linha[1:]
+    is_vencedor = verifica_vitoria_aux_linha2(head_linha, len(head_linha))
+    return is_vencedor or verifica_vitoria_aux_linha(tail_linha, n-1)
+
+def verifica_vitoria (lista_jogadores, n):
+    if(n==0):
+        return []
+    head_jogador = lista_jogadores[0]
+    tail_jogador = lista_jogadores[1:]
+    tipo = head_jogador['tipo_cartela']
+    if tipo == 'l':
+        is_vencedor = verifica_vitoria_aux_linha(head_jogador['cartela'], len(head_jogador['cartela']))
+    else:
+        # tem uma função transpose no bingo.lhs que eu copiei do stack overflow
+        cartela_transposta = list(map(list, zip(*head_jogador['cartela']))) 
+        is_vencedor = verifica_vitoria_aux_linha(cartela_transposta, len(cartela_transposta))
+    if is_vencedor == True:
+        return [head_jogador['nome']] + verifica_vitoria(tail_jogador, n-1)
+    else:
+        return verifica_vitoria(tail_jogador, n-1)
+    
 
 
 def calcula_turno(game_state):
@@ -97,27 +193,38 @@ def calcula_turno(game_state):
     new_rodada = game_state['rodada'] + 1
     old_numeros_sorteados = game_state['numeros_sorteados']
     old_numeros_para_sorteio = game_state['numeros_para_sorteio']
+    old_vencedores = game_state['vencedores']
     qtd_para_sorteio = len(old_numeros_para_sorteio)
 
-    if qtd_para_sorteio <= 0 or new_rodada > 75:
+    if qtd_para_sorteio <= 0 or new_rodada > 75 or len(old_vencedores) > 0:
+        if len(old_vencedores) > 0:
+            game_over(game_state)
         return
     else:
         num_sorteado = old_numeros_para_sorteio[0]
         new_numeros_para_sorteio = old_numeros_para_sorteio[1:]
         new_numeros_sorteados = [num_sorteado] + old_numeros_sorteados
-        print("Rodada:", new_rodada, "Numero sorteado:", num_sorteado)
+        print("******* Rodada:", new_rodada, "Numero sorteado:", num_sorteado,'*********')
 
         new_jogadores = preenche_cartela(old_jogadores, num_sorteado, len(old_jogadores))
+
+        imprime_cartela(new_jogadores, num_sorteado, len(new_jogadores))
+
+        new_vencedores = verifica_vitoria(new_jogadores, len(new_jogadores))
 
         calcula_turno({
             'jogadores': new_jogadores,
             'rodada': new_rodada,
             'numeros_sorteados': new_numeros_sorteados,
-            'numeros_para_sorteio': new_numeros_para_sorteio
+            'numeros_para_sorteio': new_numeros_para_sorteio,
+            'vencedores': new_vencedores
         })
 
 # ----------------------- Finalizacao --------------------------
 
+def game_over(game_state):
+    vencedores = game_state['vencedores']
+    print("Parabéns:",vencedores)
 # ------------------------ Main Loop ---------------------------
 
 
@@ -138,31 +245,35 @@ def main():
             'jogadores': new_jogadores,
             'rodada': new_rodada,
             'numeros_sorteados': new_numeros_sorteados,
-            'numeros_para_sorteio': new_numeros_para_sorteio
+            'numeros_para_sorteio': new_numeros_para_sorteio,
+            'vencedores': []
         })
         return
 
 
-# main()
-print(
-    preenche_cartela(
-        [
-            {
-                'cartela': [
-                    [{'numero': 1, 'ja_saiu': False}, {'numero': 4, 'ja_saiu': False}],
-                    [{'numero': 2, 'ja_saiu': False}, {'numero': 5, 'ja_saiu': True}],
-                    [{'numero': 3, 'ja_saiu': True}, {'numero': 6, 'ja_saiu': False}]
-                ]
-            },
-            {
-                'cartela': [
-                    [{'numero': 2, 'ja_saiu': False}, {'numero': 3, 'ja_saiu': True}],
-                    [{'numero': 5, 'ja_saiu': True}, {'numero': 7, 'ja_saiu': False}],
-                    [{'numero': 9, 'ja_saiu': False}, {'numero': 11, 'ja_saiu': False}]
-                ]
-            }
-        ],
-        9,
-        2
-    )
-)
+main()
+# imprime_cartela(
+#     [
+#         {
+#             'nome': 'joão',
+#             'tipo_cartela': 'l',
+#             'cartela': [
+#                 [{'numero': 1, 'ja_saiu': False}, {'numero': 4, 'ja_saiu': False}],
+#                 [{'numero': 2, 'ja_saiu': False}, {'numero': 5, 'ja_saiu': True}],
+#                 [{'numero': 3, 'ja_saiu': True}, {'numero': 6, 'ja_saiu': False}]
+#             ]
+#         },
+#         {
+#             'nome': 'maria',
+#             'tipo_cartela': 'c',
+#             'cartela': [
+#                 [{'numero': 2, 'ja_saiu': False}, {'numero': 3, 'ja_saiu': True}],
+#                 [{'numero': 5, 'ja_saiu': True}, {'numero': 7, 'ja_saiu': False}],
+#                 [{'numero': 9, 'ja_saiu': False}, {'numero': 11, 'ja_saiu': False}]
+#             ]
+#         }
+#     ],
+#     9,
+#     2
+# )
+
